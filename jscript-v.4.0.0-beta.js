ec700d614495f431c5c40393a735c062d03af36e
JS Projects <110464857+Projset@users.noreply.github.com>
2022-08-15 23:41:40 +0530

    Update jscript-v.4.0.0-beta.js

--- jscript-v.4.0.0-beta.js
+++ jscript-v.4.0.0-beta.js
@@ -1,1 +1,14 @@
+// Projset :: Jscript
+// Version :: 4.0.0 - MIT License
+/*
+    Change Highlight
+    -- jScript will be changed to Jscript
+    -- Transform to ES6 as many as possible
+*/
 
+(function( baseName ) {
+    'use strict';
+
+    baseName = 
+
+}( '$' ));
