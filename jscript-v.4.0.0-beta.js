7a0c23c21657d4e25618476427a39795521ff5e0
JS Projects <110464857+Projset@users.noreply.github.com>
2022-08-15 23:42:32 +0530

    Update jscript-v.4.0.0-beta.js

--- jscript-v.4.0.0-beta.js
+++ jscript-v.4.0.0-beta.js
@@ -9,6 +9,29 @@
 (function( baseName ) {
     'use strict';
 
-    baseName = 
+    baseName = baseName || '$';
+    // BASE for usage inside of scope
+    const BASE = {};
+
+    // Degub errors
+    BASE.debugMode = true;// Change here to prevent debugging errors
+    BASE.debug = ( ...data ) => BASE.debugMode && console.error( ...data );
+
+    // Is for managing small functions
+    const Is = {
+        array: array => array && array instanceof Array,
+        arrayLike: array => Array.isArray( array ) || ( array && array instanceof Object && array.hasOwnProperty( 'length' ) && typeof array.length === 'number' ),
+        boolean: bool => typeof bool === 'boolean',
+        element: element => element && element instanceof Element,
+        function: context => context && typeof context === 'function',
+        jScript: jScript => jScript && window.jScript && jScript instanceof window.jScript,
+        mobile: context => /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test( ( context || window ).navigator.userAgent ),
+        node: node => node && node instanceof Node,
+        number: number => typeof number === 'number',
+        parseNum: number => typeof parseInt( number ) === 'number',
+        object: object => object && object instanceof Object,
+        string: string => typeof string === 'string',
+    };
+
 
 }( '$' ));
